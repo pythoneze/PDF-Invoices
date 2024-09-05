098a4e8e3601d141258e7c1eef3a7b822b9755a2
pythoneze <pythoneze@gmail.com>
2024-09-04 20:16:33 -0400

    Add an invoice date

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ from pathlib import Path
 FILEPATHS = gb.glob("invoices/*xlsx*")
 
 
-def create_pdf(filename, invoice_number):
+def create_pdf(filename, invoice_number, date):
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_number}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_number}", ln=1)
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
-def proces_invoices():
+def process_invoices():
     for filepath in FILEPATHS:
         df = pd.read_excel(filepath, sheet_name="Sheet 1")
         filename = Path(filepath).stem
-        invoice_number = filename.split("-")[0]
-        create_pdf(filename, invoice_number)
+        invoice_number, date = filename.split("-")
+        create_pdf(filename, invoice_number, date)
 
 
 if __name__ == "__main__":
-    proces_invoices()
+    process_invoices()
